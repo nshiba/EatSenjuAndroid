45c2d76fa4961a431183412fce77f6b405ad776c
Yuichi Saito <munisystem@gmail.com>
2017-05-07 15:15:51 +0900

    Add client

--- app/src/main/java/net/nshiba/eatsenju/networks/EatSenjuService.kt
+++ app/src/main/java/net/nshiba/eatsenju/networks/EatSenjuService.kt
@@ -1,6 +1,8 @@
 package net.nshiba.eatsenju.networks
 
 import android.text.method.SingleLineTransformationMethod
+import io.reactivex.Single
+import retrofit2.http.GET
 
 interface EatSenjuService {
     @GET("macros/echo?user_content_key=o_cTUl6CMCaW0Sg2DeMpfjBvWDI6o67Nut8E0V_zKbcg8P8GkYNkpGoTJdP7Mz57Gf8Bgj9AANDal0yq0aW97sQMbkfg5V-Jm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnNqPwCBRGAH-PcBEmKBDalnk0NgqNnBCCMaefBU1k4FbNaVrruTrUcrw-jBN-VFVfNveWPraiRQB&lib=MPuixP1oMDpjlDIdT_bqVlAwJX0hRdcSS")
